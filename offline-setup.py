f0662483a662f68bc56642b0be026f91a4ff95a9
Hot Chocut <67981554+HotChocut@users.noreply.github.com>
2023-01-25 09:44:40 -0800

    Add files via upload

---
+++ offline-setup.py
@@ -0,0 +1,16 @@
+with open('launch.py', 'r') as file :
+  filedata = file.read()
+
+filedata = filedata.replace('run_pip(f\"install -r {requirements_file}\", \"requirements for Web UI\")', '#run_pip(f\"install -r {requirements_file}\", \"requirements for Web UI\")')
+
+with open('launch-offline.py', 'w') as file:
+  file.write(filedata)
+
+
+with open('webui.bat', 'r') as file :
+  filedata = file.read()
+
+filedata = filedata.replace('launch.py', 'launch-offline.py')
+
+with open('webui-offline.bat', 'w') as file:
+  file.write(filedata)

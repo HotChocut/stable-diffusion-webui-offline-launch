9247628ccaa5438bcb0dcc22d29f36a163414030
Hot Chocut <67981554+HotChocut@users.noreply.github.com>
2023-07-07 14:41:07 -0700

    Updating for latest version of webui
    They changed the location of the requirements check to inside of 'modules\launch_utils.py' on line 309 so I made it fix that as well whilst still hopefully retaining backwards compatibility for older versions of webui

--- offline-setup.py
+++ offline-setup.py
@@ -3,10 +3,22 @@ with open('launch.py', 'r') as file :
 
 filedata = filedata.replace('run_pip(f\"install -r {requirements_file}\", \"requirements for Web UI\")', '#run_pip(f\"install -r {requirements_file}\", \"requirements for Web UI\")')
 filedata = filedata.replace('run_pip(f\"install -r \\\"{requirements_file}\\\"\", \"requirements for Web UI\")', '#run_pip(f\"install -r \\\"{requirements_file}\\\"\", \"requirements for Web UI\")')
+filedata = filedata.replace('launch_utils', 'launch_utils_offline')
 
 with open('launch-offline.py', 'w') as file:
   file.write(filedata)
 
+################################################################
+
+with open('modules\launch_utils.py', 'r') as file :
+  filedata = file.read()
+
+filedata = filedata.replace('run_pip(f\"install -r \\\"{requirements_file}\\\"\", \"requirements\")', '#run_pip(f\"install -r \\\"{requirements_file}\\\"\", \"requirements\")')
+
+with open('modules\launch_utils_offline.py', 'w') as file:
+  file.write(filedata)
+
+################################################################
 
 with open('webui.bat', 'r') as file :
   filedata = file.read()
